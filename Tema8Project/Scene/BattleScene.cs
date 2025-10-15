3570cae10f740cc5ab8be42f17c33d2c1e3334ac
yun <ycy6797@naver.com>
2025-10-15 16:02:45 +0900

    Update BattleScene.cs

--- Tema8Project/Scene/BattleScene.cs
+++ Tema8Project/Scene/BattleScene.cs
@@ -2,28 +2,29 @@
 using System.Collections.Generic;
 using System.Reflection;
 using System.Security.Cryptography;
+using System.Threading;
 using TxtRPG.Data;
 using TxtRPG.Game;
 
 namespace TxtRPG.Scene
 {
-    
 
-    public class BattleStartScene: Iscene //붙여서 수정해서 푸시하기
+
+    public class BattleStartScene : Iscene
     {
-        
-        public object Run(Player player)//다시 run으로 돌려놓고 푸시하기
+
+        public object Run(Player player,Monster monster)
         {
-            ShowBattle(player);
+            ShowBattle(player,monster);
             return new TitleScene();
         }
 
         List<Monster> showMonsters = new List<Monster>()
-            {
+        {
             //반복문 안에서 선택된 몬스터 정보 저장하기 위한 저장소
-            };
+        };
 
-         List<Monster> monsters = new List<Monster>()
+        List<Monster> monsters = new List<Monster>()
             {
             new Monster("마왕", 99, 99, 99),//몬스터의 이름. 레벨, 체력, 공격력
             new Monster("사천왕", 4, 25, 40),
@@ -31,8 +32,9 @@ namespace TxtRPG.Scene
             };
 
         int showMonsterCount;
+       
 
-        private void ShowBattle(Player player)
+        private void ShowBattle(Player player, Monster monster)
         {
             Console.Clear();
 
@@ -48,24 +50,28 @@ namespace TxtRPG.Scene
             {
                 Monster enemy = monsters[randomMonsterChoice.Next(monsters.Count)];
 
-                showMonsters.Add(new Monster(enemy.monsterName,enemy.monsterLevel,enemy.monsterHp,enemy.monsterAttackPower,i + 1));// 마지막은 순서
-           
-              Console.WriteLine($"\n\nLV.{enemy.monsterLevel} {enemy.monsterName} HP {enemy.monsterHp}");
+                showMonsters.Add(new Monster(enemy.monsterName, enemy.monsterLevel, enemy.monsterHp, enemy.monsterAttackPower, i + 1));// 마지막은 순서
+
+                Console.WriteLine($"\n\nLV.{enemy.monsterLevel} {enemy.monsterName} HP {enemy.monsterHp}");
             }
 
-            //player Class에 접근해서 플레이어 정보 출력
+            int playerLevel = player.level;
+            string playername= player.name;
+            string playerJob = player.job; ;
+            int playerCurrentHP = player.hp;
+            int playerMaxHp = player.maxHp; ;
 
-            Console.WriteLine("\r\n1. 공격\r\n\r\n원하시는 행동을 입력해주세요.!");
 
-           
+            Console.WriteLine($"\\n[내정보]\nLv.{playerLevel}    {playername} ({playerJob})\nHP {playerCurrentHP}/{playerMaxHp} ");
 
+            Console.Write("\n\n\n\n1. 공격\n\n원하시는 행동을 입력해주세요.!\n>>");
 
 
             string input = Console.ReadLine();
 
             if (input == "1")
             {
-                AttackScene();
+                AttackScene(player, monster);
             }
             else
             {
@@ -76,16 +82,16 @@ namespace TxtRPG.Scene
                     input = Console.ReadLine();
                     if (input == "1")
                     {
-                        AttackScene();
+                        AttackScene(player, monster);
                     }
 
                 }
             }
         }
 
-        private void AttackScene()
+        private void AttackScene(Player player,Monster monster)
         {
-
+            Console.Clear();
             Console.WriteLine("공격할 몬스터를 선택하세요:");
 
             List<int> monsterIndexes = new List<int>();
@@ -95,23 +101,25 @@ namespace TxtRPG.Scene
                 Monster willAtkMonster = showMonsters[i];
                 Console.WriteLine($"\n\n[{willAtkMonster.indexmonster}] LV.{willAtkMonster.monsterLevel} {willAtkMonster.monsterName} HP {willAtkMonster.monsterHp}");
                 monsterIndexes.Add(willAtkMonster.indexmonster);
+
             }
 
             Console.Write("\n>> ");
             string input = Console.ReadLine();
 
 
-            if (int.TryParse(input, out int selectedIndex)) 
+            if (int.TryParse(input, out int selectIndex))
             {
-                if (monsterIndexes.Contains(selectedIndex))
+                if (selectIndex== monster.indexmonster)
                 {
-                    Monster target = showMonsters.Find(m => m.indexmonster == selectedIndex);
+                    Monster target = showMonsters.);
 
                     if (target.monsterIsAlive)
                     {
                         Console.WriteLine($"\n{target.monsterName}을(를) 공격합니다!");
-                        
-                        target.TakeDamage(10);
+
+                        target.TakeDamage(player.damage);
+
                         Console.WriteLine($"{target.monsterName} HP: {target.monsterHp}");
 
                         if (!target.monsterIsAlive)
@@ -132,8 +140,11 @@ namespace TxtRPG.Scene
                 Console.WriteLine("잘못된 입력입니다. 숫자를 입력하세요.");
             }
         }
+
+
+
+
     }
-}
 
 
     public class Monster
@@ -155,13 +166,16 @@ namespace TxtRPG.Scene
             indexmonster = indexShowMonster;
         }
 
-        public void TakeDamage(int damage)//플레이어가 입힌 피해에 따라 몬스터의 체력이 감소하며 사망하는 메서드
+        public void TakeDamage(int playerDamege)//플레이어가 입힌 피해에 따라 몬스터의 체력이 감소하며 사망하는 메서드
         {
-            monsterHp -= damage;
+
+            monsterHp -= playerDamege;
             if (monsterHp <= 0)
             {
                 monsterIsAlive = false;
+
             }
         }
 
     }
+}
