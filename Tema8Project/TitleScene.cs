84bff1afd200d109331e97c511322f1a6fda48c8
dldygksp9701-wq <dldygksp9701@gmail.com>
2025-10-15 10:59:28 +0900

    치명타 확률 회피율 등등

---
+++ Tema8Project/TitleScene.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class TitleScene
+{
+    public void Mainmenu()
+    {
+        Console.Clear();
+        Console.WriteLine(" 1. 상태보기");
+        Console.WriteLine(" 2. 전투시작");
+        Status status = new Status();
+
+        while (true)
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            if (input == 1)
+            {
+                status.view();
+            }
+
+            else if (input == 2)
+            {
+
+            }
+            else
+            {
+                Console.Write("잘못입력했습니다");
+            }
+        }
+
+
+    }
+}
